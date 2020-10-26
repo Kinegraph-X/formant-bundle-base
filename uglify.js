79e62b4ca6b6cc3d8ee260e5e50a46635924127e
vAsSiLyB <vassilyb.r@gmail.com>
2020-10-26 09:55:59 +0100

    "Uglify" is now "Terser" (ES6 compatibility) (old file removed)

--- uglify.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt, options) {
-
-	return {
-		files: {
-			'<%=pathToProject%>js/<%=currentProject%>.dist.js' : ['<%=pathToProject%>js/<%=currentProject%>.dist.js']
-		}
-	}
-};
