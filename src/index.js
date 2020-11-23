e888b6f53751d079ac98fd5fb6443a87b06fa318
vAsSiLyB <vassilyb.r@gmail.com>
2020-11-23 13:20:44 +0100

    No more reference to a "context" nor "FactoryApplicationContext" obj

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ var rDataset = require('src/core/ReactiveDataset');
 var cSet = require('src/core/ComponentSet');
 
 (function () {
-	this.factoryGlobalContext = this.factoryGlobalContext || {};
-	// globalContext context object is then implicitly kown to reside in window scope
 	// unique instance of appConstants will be initialized here, and used anywhere the instance is "got" (via getInstance() ) with globalContext as context
 	init.launch({
 		UIDPrefix : 'appName'
