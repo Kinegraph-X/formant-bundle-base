ec168a5ab0ba0dca99e5632bd096dac00c22364e
sylvain <vassilyB@gmail.com>
2021-05-28 11:02:18 +0200

    Added bundle-time-aggregated v8Launcher

--- browserify.js
+++ browserify.js
@@ -25,12 +25,32 @@ module.exports = function(grunt, options) {
 				// the inclusion in the bundle is made on the base of the file generated on the first call to the transform.
 				// But at the end of the static analysis ( + source buffer generation), the cached file has its "final" contents.
 				// So a basic Regex allows us to "re-inject" it in the bundled file.
+				
+				// We also could find useful to execute our code directly in the d8 REPL
+				// The we'd need to expose the global namespace through the "window" object
+				// 		(as some of the libs we're using are only ment to run in the browser)
+				// Let's handle that here too.
 				postBundleCB : function(err, src, next) {
 					if (err)
 						next(err, src);
 					// The next callback should be called with (err, modifiedSrc); the modifiedSrc is what will be written to the output file.
 					var stringifiedSources = grunt.file.read(options.pathToProject + 'cache/stringifiedSources.js');
 					var modifiedSrc = src.toString().replace(/module\.exports\s=\s\{sourcesAsStringArrays.*/, stringifiedSources);
+					
+					// node.js and d8 compatibility
+					var nodeOrd8Polyfill = `
+					// Get compatibility with node.js and standalone engines
+					if (typeof window === 'undefined') {
+						if (typeof global === 'undefined')
+							window = this;
+						else
+							window = global;
+					
+						window.document = (typeof d8 !== 'undefined' && d8.dom) || {};
+					}
+					`.replace(/\t{5}/g, '');
+					modifiedSrc = nodeOrd8Polyfill + modifiedSrc;
+					
 					next(err, modifiedSrc);
 				},
 				plugin: [
--- execute.js
+++ execute.js
@@ -5,7 +5,10 @@ module.exports = function(grunt, options) {
 			options: Object.assign(options, {
 				module : true
 			}),
-			src: [options.rootPath + 'jsUIFramework/src/UI/_build_helpers/_UIpackages.js'],
+			src: [
+				options.rootPath + 'jsUIFramework/src/UI/_build_helpers/_UIpackages.js',
+				options.rootPath + 'jsCore/src/_buildTools/v8Launcher.js'
+				],
 			after : function(gr, opt){
 				grunt.config.data.UIPackageList = opt.UIpackageList;
 				grunt.config.data.UIvalidatorsList = opt.UIvalidatorsList;
